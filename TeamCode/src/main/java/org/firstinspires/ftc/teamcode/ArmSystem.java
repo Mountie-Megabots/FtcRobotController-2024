fa75fde6991c3592a79d94f50f04e10a012afa15
CodeSplash347 <manchestercedrek@gmail.com>
2024-10-16 21:36:14 -0400

    Moved all arm-related code off of Teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmSystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmSystem.java
@@ -1,56 +1,103 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 
 public class ArmSystem {
     ArmSystem base;     // Reference to big arm (used by little arm for FF).
-    DcMotorEx motor;
-    DcMotorEx encoder;
-    double startingPose;
+    DcMotorEx leftBigArm, rightBigArm, smallArm, intake;
+    int bigArmEnc, smallArmEnc, armTargPos;
+    double startingPose, bigArmSpeed, smallArmSpeed, initSmallAngle, initBaseAngle, baseArmMotorSpeed, smallArmMotorSpeed;
 
 
-    public ArmSystem(DcMotorEx motor, DcMotorEx encoder, double startingPose){
-
+    public ArmSystem(HardwareMap hardwareMap, double startingPose, MecanumDrive drive){
+        leftBigArm = hardwareMap.get(DcMotorEx.class, "leftBigArm");
+        rightBigArm = hardwareMap.get(DcMotorEx.class, "rightBigArm");
+        bigArmEnc = drive.leftFront.getCurrentPosition();
+        leftBigArm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightBigArm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightBigArm.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftBigArm.setDirection(DcMotorSimple.Direction.FORWARD);
+        smallArm = hardwareMap.get(DcMotorEx.class, "smallArmMotor");
+        smallArmEnc = smallArm.getCurrentPosition();
+        intake = hardwareMap.get(DcMotorEx.class, "intake");
+        smallArm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        intake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        initBaseAngle = -37;
+        initSmallAngle = 142;
     }
 
-    public ArmSystem(DcMotorEx motor, DcMotorEx encoder, double startingPose, ArmSystem base){
-
+    /*
+    public ArmSystem(HardwareMap hardwareMap, double startingPose, ArmSystem base){
+        smallArm = hardwareMap.get(DcMotorEx.class, "smallArmMotor");
+        smallArmEnc = smallArm.getCurrentPosition();
+        intake = hardwareMap.get(DcMotorEx.class, "intake");
+        smallArm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        intake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
+    */
+    public double getPositionDegrees(double initAngle, double encoder){
+        return initAngle + encoder*0.04453015;
     }
 
-    public double getPositionDegrees(){
-        return 0;
-    }
-
-    private double getGravityFeedForward(){
-        double angle = encoder.getCurrentPosition();
+    public double getGravityFeedForward(DcMotorEx encoder, double encAngle, double smallEncAngle){
+        double initial = .25;
+        double baseAngle = initBaseAngle;
+        double angle = getPositionDegrees(initBaseAngle, encAngle);
 
         // If this Arm is connected to another arm segment
-        if(base != null){
-            angle+= base.getPositionDegrees();
+        if(encoder == smallArm){
+            baseAngle = initSmallAngle;
+            initial = .46;
+            angle = getPositionDegrees(baseAngle, smallEncAngle) + getPositionDegrees(initBaseAngle, encAngle);
+        }
+
+
+        double radians = angle*(Math.PI/180);
+
+        if(angle < 90){
+            return Math.cos(radians)*initial;
+        }
+        else{
+            return Math.cos(radians)*initial;
         }
 
         // Rest of the FeedForward code goes here.
-
-        return 0;
     }
 
     /*
         This function should be called in the robot main loop and should set motor speeds.
      */
-    public void periodic(){
+    public void periodic(Gamepad gamepad2, double pid, MecanumDrive drive){
+        smallArmEnc = smallArm.getCurrentPosition();
+        bigArmEnc = drive.leftFront.getCurrentPosition();
+        baseArmMotorSpeed = pid;
 
+        rightBigArm.setPower(baseArmMotorSpeed);
+        leftBigArm.setPower(baseArmMotorSpeed);
+        smallArm.setPower(smallArmMotorSpeed);
     }
 
     /*
         This function should be called by the main robot program to pass it's data to Telemetry.
      */
-    public void writeTelemetry(Telemetry telemetry, String caption){
-
+    public void writeTelemetry(Telemetry telemetry, double Pid){
+        telemetry.addData("Big Arm Target", armTargPos);
+        telemetry.addData("Big Arm PID", Pid);
+        telemetry.addData("Big Arm Pos",  getPositionDegrees(initBaseAngle, bigArmEnc));
+        telemetry.addData("Big Arm Power", leftBigArm.getPower());
+        telemetry.addData("FF large", getGravityFeedForward(leftBigArm, bigArmEnc, smallArmEnc));
+        telemetry.addData("Small Arm Pos",  getPositionDegrees(initSmallAngle, smallArmEnc));
+        telemetry.addData("Small Arm Power", smallArm.getPower());
+        telemetry.addData("Small Arm Real", getPositionDegrees(initSmallAngle, smallArmEnc) + getPositionDegrees(initBaseAngle, bigArmEnc));
+        telemetry.addData("FF small", getGravityFeedForward(smallArm, bigArmEnc, smallArmEnc));
     }
-
     /*
         This function changes the arm's target position.
      */
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -10,29 +10,25 @@ import org.firstinspires.ftc.teamcode.PIDController;
 
 @TeleOp(name = "Teleop")
 public class Teleop extends LinearOpMode {
-
-
-
     @Override
     public void runOpMode() throws InterruptedException {
         Pose2d beginPose = new Pose2d(0, 0, 0);
         MecanumDrive drive = new MecanumDrive(hardwareMap, beginPose);
         PIDController pid = new PIDController(.1, 0, 0);
-
+        ArmSystem armSystem = new ArmSystem(hardwareMap, -5, drive);
+        //ArmSystem smallArm = new ArmSystem(hardwareMap, -24, baseArm);
 
         waitForStart();
 
-        drive.leftBigArm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        drive.rightBigArm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        drive.smallArm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        armSystem.leftBigArm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        armSystem.rightBigArm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        armSystem.smallArm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-        drive.leftBigArm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        drive.rightBigArm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        drive.smallArm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        armSystem.leftBigArm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        armSystem.rightBigArm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        armSystem.smallArm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-
-
-
+        armSystem.armTargPos = 0;
 
         while(opModeIsActive()){
 
@@ -44,120 +40,41 @@ public class Teleop extends LinearOpMode {
 //            drive.leftBack.setPower(y - x + rx);
 //            drive.rightFront.setPower(y - x - rx);
 //            drive.rightBack.setPower(y + x - rx);
-            int bigArmEnc = drive.leftFront.getCurrentPosition();
-            int smallArmEnc = drive.smallArm.getCurrentPosition();
 
-            telemetry.addData("Big Arm pos", bigArmEnc);
-            telemetry.addData("Small Arm pos", smallArmEnc);
-            telemetry.addData("Rotation", getArmDegrees(bigArmEnc));
-            telemetry.addData("Motor Power",gamepad2.left_stick_y);
-            telemetry.addData("Small Arm Rot", getSmallArmDegrees(smallArmEnc));
-            telemetry.addData("Small Arm Real Angle", getSmallArmRealAngle(smallArmEnc, bigArmEnc));
-            telemetry.addData("Small Motor Power",drive.smallArm.getPower());
             //Large Arm
             //down pos = -34 deg -20 tick
             // straight up 90 deg 2852 tick
             // 0.043175 degrees per tick
 
-
-
             //Small Arm;
             //home 137 deg 9 tick
             // 0 deg -3100 tick
 
-//            if(Math.abs(gamepad2.left_stick_y) > .05){
-//                drive.leftBigArm.setPower(-gamepad2.left_stick_y);
-//                drive.rightBigArm.setPower(-gamepad2.left_stick_y);
-//            }
-//            else{
-//                drive.leftBigArm.setPower(getArmFeedForward(getArmDegrees(bigArmEnc)));
-//                drive.rightBigArm.setPower(getArmFeedForward(getArmDegrees(bigArmEnc)));
-//            }
-
-            double bigArmSpeed = 0;
-
-            double bigArmFF = getArmFeedForward(getArmDegrees(bigArmEnc));
-            double goal = 0;
-            telemetry.addData("FF", bigArmFF);
             if(gamepad2.left_bumper){
-                goal = 0;
+                armSystem.armTargPos = -32;
             }
             else if(gamepad2.right_bumper){
-                goal = 50;
-            }
-            else {
-                bigArmSpeed = bigArmFF;
-            }
-            double pidValue = pid.calculate(goal, getArmDegrees(bigArmEnc));
-            bigArmSpeed = pidValue;
-            telemetry.addData("PID Output", pidValue);
-            drive.leftBigArm.setPower(bigArmSpeed);
-            drive.rightBigArm.setPower(bigArmSpeed);
-
-
-            //drive.smallArm.setPower(gamepad2.right_stick_y/1.35);
-            drive.smallArm.getPower();
-            if(Math.abs(gamepad2.right_stick_y) > 0.05){
-                drive.smallArm.setPower(gamepad2.right_stick_y/1.35);
+               armSystem.armTargPos = 75;
             } else {
-                drive.smallArm.setPower(getSmallArmFeedForward(getSmallArmRealAngle(smallArmEnc,bigArmEnc)));
+                armSystem.baseArmMotorSpeed = armSystem.getGravityFeedForward(armSystem.leftBigArm, armSystem.bigArmEnc, armSystem.smallArmEnc);
             }
 
-
-
-            drive.intake.setPower(gamepad2.right_trigger);
-
-            if(gamepad2.left_trigger >= 0.75){
-
+            if(Math.abs(gamepad2.right_stick_y) > 0.05){
+                armSystem.smallArmMotorSpeed = gamepad2.right_stick_y/1.35;
+            } else {
+                armSystem.smallArmMotorSpeed = armSystem.getGravityFeedForward(armSystem.smallArm, armSystem.bigArmEnc, armSystem.smallArmEnc);
             }
 
+            double pidValue = pid.calculate(armSystem.armTargPos, armSystem.getPositionDegrees(armSystem.initBaseAngle, armSystem.bigArmEnc));
 
+            armSystem.periodic(gamepad2, pidValue, drive);
+
+            armSystem.intake.setPower(gamepad2.right_trigger);
+            armSystem.intake.setPower(-gamepad2.left_trigger);
+
+            armSystem.writeTelemetry(telemetry, pidValue);
 
             telemetry.update();
-
-
-        }
-    }
-
-    double getArmDegrees(int encoder){
-        double intitialArmAngle = -37;
-        return intitialArmAngle + encoder*0.04453015;
-    }
-
-    double getSmallArmDegrees(int encoder){
-        double initialSmallArmAngle = 142;
-        return initialSmallArmAngle + encoder*0.04453015;
-    }
-
-    double getSmallArmRealAngle(int small, int big){
-         return getSmallArmDegrees(small) + getArmDegrees(big);
-    }
-
-
-
-    double getArmFeedForward(double angle){
-        double initial = .25;
-
-        double radians = angle*(Math.PI/180);
-
-        if(angle < 90){
-            return Math.cos(radians)*initial;
-        }
-        else{
-            return Math.cos(radians)*initial;
-        }
-    }
-
-    double getSmallArmFeedForward(double angle){
-        double initial = .46;
-
-        double radians = angle*(Math.PI/180);
-
-        if(angle < 90){
-            return Math.cos(radians)*initial;
-        }
-        else{
-            return Math.cos(radians)*initial;
         }
     }
 }
